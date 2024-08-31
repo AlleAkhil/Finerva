7a4d57f29e637d93918d7d3e629ece9295cb0b6e
alleakhil <akhilalle3510@gmail.com>
2024-08-31 10:15:50 +0530

    Update

---
+++ lib/firebase_keys.dart
@@ -0,0 +1,51 @@
+// lib/firebase_keys.dart
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class FirebaseKeys {
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'AIzaSyArZFs9vacxpoWa8xZNcT747lqPBKoRrPw',
+    appId: '1:932044794561:web:7454779127902ebf820240',
+    messagingSenderId: '932044794561',
+    projectId: 'finervadb',
+    authDomain: 'finervadb.firebaseapp.com',
+    storageBucket: 'finervadb.appspot.com',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyCJFX_eeOrESkWvkJHD9YNAiWYn3q58W5g',
+    appId: '1:932044794561:android:fb2dd85759b054d7820240',
+    messagingSenderId: '932044794561',
+    projectId: 'finervadb',
+    storageBucket: 'finervadb.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyBAt2CTfDLeOaZ0MF5wNHHYmQSFJCob4bI',
+    appId: '1:932044794561:ios:54e45247171aac01820240',
+    messagingSenderId: '932044794561',
+    projectId: 'finervadb',
+    storageBucket: 'finervadb.appspot.com',
+    iosClientId: '932044794561-8g7ccir6q6o0eufdv39hn87q4v2f7j66.apps.googleusercontent.com',
+    iosBundleId: 'com.example.finerva',
+  );
+
+  static const FirebaseOptions macos = FirebaseOptions(
+    apiKey: 'AIzaSyBAt2CTfDLeOaZ0MF5wNHHYmQSFJCob4bI',
+    appId: '1:932044794561:ios:54e45247171aac01820240',
+    messagingSenderId: '932044794561',
+    projectId: 'finervadb',
+    storageBucket: 'finervadb.appspot.com',
+    iosClientId: '932044794561-8g7ccir6q6o0eufdv39hn87q4v2f7j66.apps.googleusercontent.com',
+    iosBundleId: 'com.example.finerva',
+  );
+
+  static const FirebaseOptions windows = FirebaseOptions(
+    apiKey: 'AIzaSyArZFs9vacxpoWa8xZNcT747lqPBKoRrPw',
+    appId: '1:932044794561:web:fffe5042ee626a42820240',
+    messagingSenderId: '932044794561',
+    projectId: 'finervadb',
+    authDomain: 'finervadb.firebaseapp.com',
+    storageBucket: 'finervadb.appspot.com',
+  );
+}
